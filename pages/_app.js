4ff5aa7ceab49a4ff18a503b48ad7f7a7620fe85
mayanhavoc <mayanhavoc@gmail.com>
2022-09-26 10:17:32 -0700

    add chakra provider and change theme styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,43 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ChakraProvider, extendTheme, Box } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#f7fafc",
+      900: "#1a202c",
+    },
+  },
+  fonts: {
+    heading: "Work Sans",
+    body: "Comfortaa"
+  },
+  components: {
+    Heading: {
+      baseStyle: {
+        color: "purple.800"
+      },
+      variants: {
+        'page-heading': {
+          color: "red.900",
+          py: "4"
+        }
+      }
+    },
+    Link: {
+      baseStyle: {
+        color: "orange.700"
+      }
+    },
+  }
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <ChakraProvider theme={theme}>
+    <Component {...pageProps} />
+  </ChakraProvider>
+  )
 }
 
 export default MyApp
