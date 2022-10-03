fbfc5b34315af5e3370f6ce00227f3c5a851dcd2
mayanhavoc <mayanhavoc@gmail.com>
2022-10-02 21:18:36 -0700

    api setup

---
+++ pages/api/_recipeList.js
@@ -0,0 +1,5 @@
+// req = HTTP incoming message, res = HTTP server response
+export default function handler(req, res) {
+    const recipeName = req.body.recipeName;
+    const reference = req.body.reference;
+  }
